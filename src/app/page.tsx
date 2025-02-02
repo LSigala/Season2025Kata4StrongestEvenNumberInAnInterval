1fee551294739182c4d8026d586147a21067e510
LSigala <lsigala@arkusnexus.com>
2025-02-01 23:03:45 -0600

    Season2025Kata4StrongestEvenNumberInAnInterval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  async function strongestNumber(e: any){
+    e.preventDefault();
+
+    const data = {
+      Numbers: (e.target.numbers.value)
+    }
+    data.Numbers = data.Numbers.split(",")
+    let counts = 0
+    let countsSaved = 0
+    let number = 0
+    let validate = 0
+    let n = data.Numbers[0]
+    let m = data.Numbers[1]
+      
+    do {
+      let checkN = n
+      while (checkN > 0 && !(checkN & 1)){
+        checkN >>=1
+        counts++
+        countsSaved = counts
+      }
+
+      if (countsSaved >= validate) {
+        validate = countsSaved
+        if(n % 2 == 0){
+          number = n
+        }
+      }
+      
+      counts = 0
+      countsSaved = 0
+      n++
+        
+    } while (n <= m);
+  
+    console.log(number) 
+}
+
+
+function obtain(checkN:any){
+  let counts = 0
+  let countsSaved = 0
+  while (checkN % 2 == 0){
+    checkN >>=1
+    counts++
+    countsSaved = counts
+  }
+  return counts
+}
+  return (
+      <form onSubmit={strongestNumber} className="container col-6 text-center mt-4 bg-light">
+        <div>
+            <div className="d-flex justify-content-center form-group">
+                <div className="form-group col-5 px-md-4 mb-3">
+                    <label>Gloves</label>
+                    <input type="text" className="form-control" id="numbers" required aria-describedby="emailHelp " />
+                </div>
+            </div>
+            <button type="submit" className="btn btn-primary mt-3 mx-auto d-block">Send Recipe</button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </form>
   );
 }
